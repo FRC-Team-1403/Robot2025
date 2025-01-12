c209b6f1d79223fe260f6df9651d5181540ce2ff
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2025-01-11 19:15:11 -0500

    optimize limelight helpers

--- src/main/java/team1403/robot/vision/LimelightHelpers.java
+++ src/main/java/team1403/robot/vision/LimelightHelpers.java
@@ -544,7 +544,7 @@ public class LimelightHelpers {
          * Instantiates a PoseEstimate object with default values
          */
         public PoseEstimate() {
-            this.pose = new Pose2d();
+            this.pose = Pose2d.kZero;
             this.timestampSeconds = 0;
             this.latency = 0;
             this.tagCount = 0;
@@ -596,7 +596,7 @@ public class LimelightHelpers {
         if(inData.length < 6)
         {
             //System.err.println("Bad LL 3D Pose Data!");
-            return new Pose3d();
+            return Pose3d.kZero;
         }
         return new Pose3d(
             new Translation3d(inData[0], inData[1], inData[2]),
@@ -615,7 +615,7 @@ public class LimelightHelpers {
         if(inData.length < 6)
         {
             //System.err.println("Bad LL 2D Pose Data!");
-            return new Pose2d();
+            return Pose2d.kZero;
         }
         Translation2d tran2d = new Translation2d(inData[0], inData[1]);
         Rotation2d r2d = new Rotation2d(Units.degreesToRadians(inData[5]));
