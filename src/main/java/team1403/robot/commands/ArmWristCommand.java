cda91259d7dac5fe9f1c00b2ddd68e18322517f5
tinyrice01 <widee135@gmail.com>
2025-01-13 16:39:43 -0500

    created arm and wrist command

---
+++ src/main/java/team1403/robot/commands/ArmWristCommand.java
@@ -0,0 +1,40 @@
+package team1403.robot.commands;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import team1403.robot.subsystems.Arm;
+import team1403.robot.subsystems.Wrist;
+import team1403.robot.Constants;
+
+public class ArmWristCommand extends Command {
+    private Arm m_arm;
+    private Wrist m_wrist;
+    private BooleanSupplier m_up;
+    private BooleanSupplier m_down; 
+    private XboxController m_ops;
+
+    public ArmWristCommand(Arm arm, Wrist wrist, XboxController ops, BooleanSupplier up, BooleanSupplier down) {
+        m_arm = arm;
+        m_wrist = wrist;
+        m_up = up;
+        m_down = down;
+        m_ops = ops;
+
+        addRequirements(m_arm, m_wrist);
+    }
+
+    @Override
+    public void execute() {
+        if (m_up.getAsBoolean()) {
+            m_arm.setArmSetpoint(Constants.Arm.kDriveSetpoint);
+            m_wrist.setWristSetpoint(Constants.Wrist.kDriveSetpoint);
+        }
+        if (m_down.getAsBoolean()) {
+            m_arm.setArmSetpoint(Constants.Arm.kIntakeSetpoint);
+            m_wrist.setWristSetpoint(0);
+        }
+    }
+}
