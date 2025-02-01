df6c468f01a2d05bc6e7348348743d52d00349be
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2025-01-31 23:00:43 -0500

    fix: set loop time in LoggedRobot constructor.

--- src/main/java/team1403/robot/Robot.java
+++ src/main/java/team1403/robot/Robot.java
@@ -34,6 +34,7 @@ public class Robot extends LoggedRobot {
    * initialization code.
    */
   public Robot() {
+    super(Constants.kLoopTime);
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     if(isReal()) {
