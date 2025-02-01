d5994257fb82e4dd60f2cccd7fb94e8c7cac98ae
Atharv - Prog <atvkumar10@gmail.com>
2025-02-01 10:50:45 -0500

    Update CoralIntakeCommand.java

--- src/main/java/team1403/robot/commands/Coral/CoralIntakeCommand.java
+++ src/main/java/team1403/robot/commands/Coral/CoralIntakeCommand.java
@@ -5,6 +5,20 @@ import team1403.robot.Constants;
 import team1403.robot.subsystems.Coral_Intake;
 
 
+// 15.875 inches is the distance 
+
+public class CoralIntakeCommand extends Command{
+    Coral_Intake m_subsystem;
+
+    public CoralIntakeCommand(Coral_Intake subsystem) {
+    m_subsystem = subsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(subsystem);
+  }
+
+
+    public void initialize() {
+    }
 
     public void execute() {
         m_subsystem.setMotorSpeed(0.3);
