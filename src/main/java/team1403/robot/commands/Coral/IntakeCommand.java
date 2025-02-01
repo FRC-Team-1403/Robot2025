6ed28a3ab4eeaccbaadaf94f9ff27a356a6aee0b
Navneet <navneet.aiyer@mtsdstudent.us>
2025-02-01 11:58:17 -0500

    Continue to work endeffector

--- src/main/java/team1403/robot/commands/Coral/IntakeCommand.java
+++ src/main/java/team1403/robot/commands/Coral/IntakeCommand.java
@@ -21,11 +21,19 @@ public class IntakeCommand extends Command{
     }
 
     public void execute() {
-        m_subsystem.setMotorSpeed(0.3);
+        m_subsystem.setIntakeMotorSpeed(0.3);
     } 
 
     public void end(boolean interrupted) {
-        m_subsystem.setMotorSpeed(0);
+        m_subsystem.setIntakeMotorSpeed(0);
+        while (m_subsystem.getEncoderValue() != 0) {
+            if (m_subsystem.getEncoderValue() > 0) {
+                m_subsystem.setWristMotorSpeed(-0.3);
+            } else if (m_subsystem.getEncoderValue() < 0) {
+                m_subsystem.setWristMotorSpeed(0.3);
+            }
+        }
+        // Wrist start position is at 90???
     }
 
     public boolean isFinished() {
