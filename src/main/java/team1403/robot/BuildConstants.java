f3ab0343e785ba58cd58be6d74f9950909d7159a
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2025-02-07 17:59:02 -0500

    added intake code

---
+++ src/main/java/team1403/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package team1403.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 98;
+  public static final String GIT_SHA = "0023cf53f2ddf2fcc50bfd482004d185823c3e9b";
+  public static final String GIT_DATE = "2025-02-02 19:51:37 EST";
+  public static final String GIT_BRANCH = "dev";
+  public static final String BUILD_DATE = "2025-02-03 18:34:52 EST";
+  public static final long BUILD_UNIX_TIME = 1738625692037L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
