a52a3e944ce0135095f4e3034fe5cb1017dbae04
devastate347 <ayaansachdeva1000@gmail.com>
2025-02-15 11:36:09 -0500

    finished merge commit

--- src/main/java/team1403/robot/swerve/SwerveSubsystem.java
+++ src/main/java/team1403/robot/swerve/SwerveSubsystem.java
@@ -2,6 +2,8 @@ package team1403.robot.swerve;
 
 import java.util.ArrayList;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathfindingCommand;
 import com.pathplanner.lib.config.PIDConstants;
@@ -22,6 +24,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
