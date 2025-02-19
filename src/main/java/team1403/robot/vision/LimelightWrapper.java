e59078163d42e12b029a811bd151507227b6830b
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2025-02-19 00:08:14 -0500

    add alert for limelight disconnect

--- src/main/java/team1403/robot/vision/LimelightWrapper.java
+++ src/main/java/team1403/robot/vision/LimelightWrapper.java
@@ -12,23 +12,27 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import team1403.robot.Constants;
 
 //import frc.robot.LimelightHelpers; 
 
 public class LimelightWrapper extends SubsystemBase implements ITagCamera {
-    private String m_name;
-    private Supplier<Rotation3d> m_imuRotation;
-    private Supplier<Transform3d> m_camTransform;
+    private final String m_name;
+    private final Supplier<Rotation3d> m_imuRotation;
+    private final Supplier<Transform3d> m_camTransform;
     private LimelightHelpers.PoseEstimate m_poseEstimate;
     private final static Matrix<N3, N1> kDefaultStdv = VecBuilder.fill(2, 2, 3);
+    private final Alert m_camDisconnected;
     
 
     public LimelightWrapper(String name, Supplier<Transform3d> cameraTransform, Supplier<Rotation3d> imuRotation) {
         m_name = name.toLowerCase(); //hostname must be lowercase
         m_imuRotation = imuRotation;
         m_camTransform = cameraTransform;
+        m_camDisconnected = new Alert("Limelight " + m_name + " Disconnected!", AlertType.kError);
         m_poseEstimate = null;
     }
     
@@ -97,6 +101,9 @@ public class LimelightWrapper extends SubsystemBase implements ITagCamera {
         LimelightHelpers.SetRobotOrientation(m_name, m_imuRotation.get());
         LimelightHelpers.setCameraPose_RobotSpace(m_name, m_camTransform.get());
         m_poseEstimate = LimelightHelpers.getBotPoseEstimate_wpiBlue(m_name);
+
+        //TODO: Detect limelight disconnect
+        m_camDisconnected.set(false);
         
         Logger.recordOutput(m_name + "/hasPose", hasPose());
 
