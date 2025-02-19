ed9f11c2a49f08d6947bc53b9e5d8b9820b0319d
devastate347 <ayaansachdeva1000@gmail.com>
2025-02-18 19:34:29 -0500

    wrist code

--- src/main/java/team1403/robot/RobotContainer.java
+++ src/main/java/team1403/robot/RobotContainer.java
@@ -162,6 +162,13 @@ public class RobotContainer {
     m_climberSubsystem.setDefaultCommand(new ClimberCommand(m_climberSubsystem, 
       () -> m_operatorController.getHID().getAButtonPressed(), 
       () -> m_operatorController.getHID().getBButtonPressed(), 0.1));
+    m_wristCommand = new WristCommand(m_wrist, m_operatorController.a(),
+      m_operatorController.b(), m_operatorController.x());
+
+    m_wrist.setDefaultCommand(m_wristCommand);
+
+
+
     
   }
 
