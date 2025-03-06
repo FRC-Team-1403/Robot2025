8035f341bac03f26d888046a5d9918d6ceb9d583
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2025-03-06 16:10:53 -0500

    new camera dimensions

--- src/main/java/team1403/robot/Constants.java
+++ src/main/java/team1403/robot/Constants.java
@@ -92,8 +92,8 @@ public class Constants {
     //left-to-right-disp = 0 inches
     //top-to-bottom disp = 17.82426 inches
     //public static final Rotation3d kCameraRotation = new Rotation3d(Math.PI, Units.degreesToRadians(-25), Math.PI);
-    public static final Rotation3d kLimelightRotation = new Rotation3d(Units.degreesToRadians(90), 0, Units.degreesToRadians(9));
-    public static final Translation3d kCameraOffset = new Translation3d(Units.inchesToMeters(4.75),Units.inchesToMeters(-12.99),Units.inchesToMeters(30.5));
+    public static final Rotation3d kLimelightRotation = new Rotation3d(Units.degreesToRadians(0), Units.degreesToRadians(7), 0); //double chekc if yaw/pitch/roll
+    public static final Translation3d kCameraOffset = new Translation3d(Units.inchesToMeters(-3.213),Units.inchesToMeters(0.495),Units.inchesToMeters(14.246)); //check negative signs 
     //public static final Transform3d kCameraTransfrom = new Transform3d(kCameraOffset, kCameraRotation);
     public static final Transform3d kLimelightTransform = new Transform3d(kCameraOffset, kLimelightRotation);
 
