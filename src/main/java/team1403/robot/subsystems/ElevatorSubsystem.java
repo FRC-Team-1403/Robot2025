b881117407e075c12e386c1f94149d1d1949124a
Atharv - Prog <atvkumar10@gmail.com>
2025-03-25 21:24:29 -0400

    Removed Striped Animation + Added Upwards and Downwards lights on elevator move

--- src/main/java/team1403/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/team1403/robot/subsystems/ElevatorSubsystem.java
@@ -26,200 +26,200 @@ public class ElevatorSubsystem extends SubsystemBase {
   private SparkMax m_leftMotor;
   private SparkMax m_rightMotor;
   private ElevatorFeedforward m_ElevatorFeedforward;
-  private double currentPos;
-  private double currMotorOutput;
-  private double desiredMotorOutput;
-  private boolean isRampDone;
-  private boolean isGoingUp;
-  private boolean isGoingDown;
-  private boolean directionFlag;
-  private double posError;
-  private double setpoint;
-  private double minSpeed;
-  private double maxSpeed;
-
-  private SysIdRoutine m_sysIdRoutine;
-
-  public ElevatorSubsystem() {
-    m_leftMotor = new SparkMax(Constants.CanBus.leftElevatorMotorID, MotorType.kBrushless);
-    m_rightMotor = new SparkMax(Constants.CanBus.rightElevatorMotorID, MotorType.kBrushless);
-    configMotors();
-
-    m_ElevatorFeedforward = new ElevatorFeedforward(0, Constants.Elevator.kFeedforwardG, Constants.Elevator.kFeedforwardV, 0, Constants.kLoopTime);
- 
-    m_sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(null, null, null, 
-      (state) -> Logger.recordOutput("SysIDElevatorFeedforward", state.toString())),
-    new SysIdRoutine.Mechanism((voltage) -> {
-      m_rightMotor.setVoltage(voltage.in(Volts));
-    }, null, this));  
-
-}
-
-   private void configMotors() {
-    SparkMaxConfig leftconfig = new SparkMaxConfig();
-    leftconfig
-        .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(45)
-        .voltageCompensation(12)
-        .follow(m_rightMotor, true);
-    SparkMaxConfig rightconfig = new SparkMaxConfig();
-    rightconfig
-        .idleMode(IdleMode.kBrake)
-        .smartCurrentLimit(45)
-        .voltageCompensation(12)
-        .inverted(true);
-
-    m_leftMotor.configure(leftconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_rightMotor.configure(rightconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-   }
-  
-  public void setMotorSpeed(double speed) {
-    m_rightMotor.set(MathUtil.clamp(speed, -1.0, 1.0));
-  }
-
-  public void stopMotors() {
-    setMotorSpeed(0);
-  }
-  
-  public double getSpeed() {
-    return m_rightMotor.getEncoder().getVelocity();
-  }
-
-  public double getPosition() {
-    return m_rightMotor.getEncoder().getPosition();
-  }
-
-  public double FFcalculation() {
-    return m_ElevatorFeedforward.calculate(0);
-  }
-
-  public Command getSysIDQ(SysIdRoutine.Direction dir) {
-    return m_sysIdRoutine.quasistatic(dir);
-  }
-
-  public Command getSysIDD(SysIdRoutine.Direction dir) {
-    return m_sysIdRoutine.dynamic(dir);
-  }
-
-    public void moveToSetpoint(double setPoint) {
-        //setpoint = setPoint;
-        safeMove(setPoint);
-    }
-
-    public void safeMove(double target) {
-        if(Blackbox.isElevatorSafe())
-            setpoint = target;
-        setpoint = Math.max(setpoint, Constants.Elevator.Setpoints.Min);
-    }
-
-    public double getSetpoint() {
-        return setpoint;
-    }
-
-    //check whether component is moving up or down
-    private void checkDirection(double setPoint) {
-        if (currentPos < 1.0) {
-            isGoingUp = false;
-            isGoingDown = false;
+  private static double currentPos;
+    private double currMotorOutput;
+    private double desiredMotorOutput;
+    private boolean isRampDone;
+    public static boolean isGoingUp;
+    private boolean isGoingDown;
+    private boolean directionFlag;
+    private double posError;
+    private static double setpoint;
+        private double minSpeed;
+        private double maxSpeed;
+      
+        private SysIdRoutine m_sysIdRoutine;
+      
+        public ElevatorSubsystem() {
+          m_leftMotor = new SparkMax(Constants.CanBus.leftElevatorMotorID, MotorType.kBrushless);
+          m_rightMotor = new SparkMax(Constants.CanBus.rightElevatorMotorID, MotorType.kBrushless);
+          configMotors();
+      
+          m_ElevatorFeedforward = new ElevatorFeedforward(0, Constants.Elevator.kFeedforwardG, Constants.Elevator.kFeedforwardV, 0, Constants.kLoopTime);
+       
+          m_sysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(null, null, null, 
+            (state) -> Logger.recordOutput("SysIDElevatorFeedforward", state.toString())),
+          new SysIdRoutine.Mechanism((voltage) -> {
+            m_rightMotor.setVoltage(voltage.in(Volts));
+          }, null, this));  
+      
+      }
+      
+         private void configMotors() {
+          SparkMaxConfig leftconfig = new SparkMaxConfig();
+          leftconfig
+              .idleMode(IdleMode.kBrake)
+              .smartCurrentLimit(45)
+              .voltageCompensation(12)
+              .follow(m_rightMotor, true);
+          SparkMaxConfig rightconfig = new SparkMaxConfig();
+          rightconfig
+              .idleMode(IdleMode.kBrake)
+              .smartCurrentLimit(45)
+              .voltageCompensation(12)
+              .inverted(true);
+      
+          m_leftMotor.configure(leftconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+          m_rightMotor.configure(rightconfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+         }
+        
+        public void setMotorSpeed(double speed) {
+          m_rightMotor.set(MathUtil.clamp(speed, -1.0, 1.0));
         }
-        else if(setPoint > currentPos + Constants.Elevator.Command.setPointMargin) {
-            isGoingUp = true;
-            isGoingDown = false;
-        } 
-        else if(setPoint < currentPos - Constants.Elevator.Command.setPointMargin) {
-            isGoingUp = false;
-            isGoingDown = true;
-        } 
-        else {
-            isGoingUp = false;
-            isGoingDown = false;
-        } 
-    }
-    
-    private double getDesiredOutput(double setPoint) {
-        // set desired motor output equal to the difference between current position and setpoint * a gain constant
-        posError = setPoint - currentPos;
-        if(isGoingUp) {
-            minSpeed = Constants.Elevator.Command.upMinSpeed;
-            maxSpeed = Constants.Elevator.Command.upMaxSpeed;
-            posError *= Constants.Elevator.Command.movementUpGain;
-        } 
-        else if(isGoingDown){
-            minSpeed = Constants.Elevator.Command.downMinSpeed;
-            maxSpeed = Constants.Elevator.Command.downMaxSpeed;
-            posError *= Constants.Elevator.Command.movementDownGain;
+      
+        public void stopMotors() {
+          setMotorSpeed(0);
         }
-        double desiredOutput = posError;
-        // checks conditions that don't require any output by the motor 
-        if(!isGoingUp && !isGoingDown || (isGoingUp && desiredOutput < 0) || (isGoingDown && desiredOutput > 0)) {
-            desiredOutput = 0;
+        
+        public double getSpeed() {
+          return m_rightMotor.getEncoder().getVelocity();
         }
-        // clamp desired motor output to a maximum value
-        desiredOutput = Math.abs(desiredOutput);
-        if(desiredOutput > maxSpeed) {
-            desiredOutput = maxSpeed;
-        }      
-        return desiredOutput;
-    }
-
-    // ramp function gradually brings up the output of the motor to the desired motor output
-    private double ramp(double rampUpTime, double rampDownTime, double currentOutput, double desiredOutput) {
-        // if desired output is greater than current output, run upwards ramp function 
-        if(desiredOutput > currentOutput) {
-        // increment current output by 100/rampUpTime/cycle rate and if that output is less than desired 
-            if((currentOutput + (100/(rampUpTime/0.02)) < desiredOutput)) {
-                currentOutput += (100/(rampUpTime/0.02));
-            }
-            // set current output to desired output
-            else {
-                currentOutput = desiredOutput;
-            }
+      
+        public double getPosition() {
+          return m_rightMotor.getEncoder().getPosition();
         }
-        // if desired output is less than current output, run downwards ramp function 
-        else if(desiredOutput < currentOutput) {
-            if((currentOutput - (100/(rampDownTime/0.02)) > desiredOutput)) {
-                // decrement current output by 100/rampDownTime/cycle rate and if that output is less than desired 
-                currentOutput -= (100/(rampDownTime/0.02));
-            }
-            else {
-                currentOutput = desiredOutput;
-            }
+      
+        public double FFcalculation() {
+          return m_ElevatorFeedforward.calculate(0);
         }
-        // if current motor output reaches the desired output set isRampDone to true
-        if(desiredOutput == currentOutput) {
-            isRampDone = true;
+      
+        public Command getSysIDQ(SysIdRoutine.Direction dir) {
+          return m_sysIdRoutine.quasistatic(dir);
         }
-        // returns our current output
-        return currentOutput;
-    }
-
-    private void adjustCurrentOutput() {
-        // once ramp function is done and the elevator is moving up or down, set velocity to a minimum value
-        if((isGoingUp || isGoingDown) && isRampDone && currMotorOutput < minSpeed) {
-            currMotorOutput = minSpeed;
+      
+        public Command getSysIDD(SysIdRoutine.Direction dir) {
+          return m_sysIdRoutine.dynamic(dir);
         }
-        // invert output if elevator is moving down
-        if(isGoingDown) {
-            currMotorOutput *= -1;
-        }
-    }
-
-    private void checkIfReachedSetPoint(double setPoint) {
-        // if elevator is within the window of the setpoint, stop the motor from running and set booleans to false
-        if(currentPos > setPoint - Constants.Elevator.Command.setPointMargin && currentPos < setPoint + Constants.Elevator.Command.setPointMargin) {
-            currMotorOutput = 0;
-            isGoingUp = false;
-            isGoingDown = false;
-            directionFlag = true;
-        }
-        // else set isRampDone to false and continue the following steps above 
-        else {
-            isRampDone = false;
-        }
-    }
-
-    public boolean isAtSetpoint() {
-        return Math.abs(setpoint - currentPos) <= 3;
+      
+          public void moveToSetpoint(double setPoint) {
+              //setpoint = setPoint;
+              safeMove(setPoint);
+          }
+      
+          public void safeMove(double target) {
+              if(Blackbox.isElevatorSafe())
+                  setpoint = target;
+              setpoint = Math.max(setpoint, Constants.Elevator.Setpoints.Min);
+          }
+      
+          public double getSetpoint() {
+              return setpoint;
+          }
+      
+          //check whether component is moving up or down
+          private void checkDirection(double setPoint) {
+              if (currentPos < 1.0) {
+                  isGoingUp = false;
+                  isGoingDown = false;
+              }
+              else if(setPoint > currentPos + Constants.Elevator.Command.setPointMargin) {
+                  isGoingUp = true;
+                  isGoingDown = false;
+              } 
+              else if(setPoint < currentPos - Constants.Elevator.Command.setPointMargin) {
+                  isGoingUp = false;
+                  isGoingDown = true;
+              } 
+              else {
+                  isGoingUp = false;
+                  isGoingDown = false;
+              } 
+          }
+          
+          private double getDesiredOutput(double setPoint) {
+              // set desired motor output equal to the difference between current position and setpoint * a gain constant
+              posError = setPoint - currentPos;
+              if(isGoingUp) {
+                  minSpeed = Constants.Elevator.Command.upMinSpeed;
+                  maxSpeed = Constants.Elevator.Command.upMaxSpeed;
+                  posError *= Constants.Elevator.Command.movementUpGain;
+              } 
+              else if(isGoingDown){
+                  minSpeed = Constants.Elevator.Command.downMinSpeed;
+                  maxSpeed = Constants.Elevator.Command.downMaxSpeed;
+                  posError *= Constants.Elevator.Command.movementDownGain;
+              }
+              double desiredOutput = posError;
+              // checks conditions that don't require any output by the motor 
+              if(!isGoingUp && !isGoingDown || (isGoingUp && desiredOutput < 0) || (isGoingDown && desiredOutput > 0)) {
+                  desiredOutput = 0;
+              }
+              // clamp desired motor output to a maximum value
+              desiredOutput = Math.abs(desiredOutput);
+              if(desiredOutput > maxSpeed) {
+                  desiredOutput = maxSpeed;
+              }      
+              return desiredOutput;
+          }
+      
+          // ramp function gradually brings up the output of the motor to the desired motor output
+          private double ramp(double rampUpTime, double rampDownTime, double currentOutput, double desiredOutput) {
+              // if desired output is greater than current output, run upwards ramp function 
+              if(desiredOutput > currentOutput) {
+              // increment current output by 100/rampUpTime/cycle rate and if that output is less than desired 
+                  if((currentOutput + (100/(rampUpTime/0.02)) < desiredOutput)) {
+                      currentOutput += (100/(rampUpTime/0.02));
+                  }
+                  // set current output to desired output
+                  else {
+                      currentOutput = desiredOutput;
+                  }
+              }
+              // if desired output is less than current output, run downwards ramp function 
+              else if(desiredOutput < currentOutput) {
+                  if((currentOutput - (100/(rampDownTime/0.02)) > desiredOutput)) {
+                      // decrement current output by 100/rampDownTime/cycle rate and if that output is less than desired 
+                      currentOutput -= (100/(rampDownTime/0.02));
+                  }
+                  else {
+                      currentOutput = desiredOutput;
+                  }
+              }
+              // if current motor output reaches the desired output set isRampDone to true
+              if(desiredOutput == currentOutput) {
+                  isRampDone = true;
+              }
+              // returns our current output
+              return currentOutput;
+          }
+      
+          private void adjustCurrentOutput() {
+              // once ramp function is done and the elevator is moving up or down, set velocity to a minimum value
+              if((isGoingUp || isGoingDown) && isRampDone && currMotorOutput < minSpeed) {
+                  currMotorOutput = minSpeed;
+              }
+              // invert output if elevator is moving down
+              if(isGoingDown) {
+                  currMotorOutput *= -1;
+              }
+          }
+      
+          private void checkIfReachedSetPoint(double setPoint) {
+              // if elevator is within the window of the setpoint, stop the motor from running and set booleans to false
+              if(currentPos > setPoint - Constants.Elevator.Command.setPointMargin && currentPos < setPoint + Constants.Elevator.Command.setPointMargin) {
+                  currMotorOutput = 0;
+                  isGoingUp = false;
+                  isGoingDown = false;
+                  directionFlag = true;
+              }
+              // else set isRampDone to false and continue the following steps above 
+              else {
+                  isRampDone = false;
+              }
+          }
+      
+          public static boolean isAtSetpoint() {
+              return Math.abs(setpoint - currentPos) <= 3;
     }
 
     public void periodic() {
