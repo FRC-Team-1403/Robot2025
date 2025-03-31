4e4b0a602d239f9df7e3a2b60d2fae4f0e92b472
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2025-03-30 22:14:42 -0400

    L4 teleop statemachine tweak

--- src/main/java/team1403/robot/commands/StateMachine.java
+++ src/main/java/team1403/robot/commands/StateMachine.java
@@ -47,7 +47,7 @@ public class StateMachine extends Command {
                 }
                 break;
             case driving: 
-                if(Blackbox.reefLevel != Blackbox.reefLevel.drive)
+                if(Blackbox.reefLevel != Blackbox.ReefScoreLevel.drive)
                     m_wristSubsystem.moveToSetpoint(Constants.Wrist.Setpoints.Drive);
                 if(Blackbox.isAligning() && Blackbox.reefLevel != Blackbox.ReefScoreLevel.drive)
                     Blackbox.robotState = State.aligning;
@@ -55,7 +55,7 @@ public class StateMachine extends Command {
             case aligning:
                 if(Blackbox.isAligning() && Blackbox.getCloseAlign(m_swerve.getPose()) && Blackbox.reefLevel == Blackbox.ReefScoreLevel.L4)
                     Blackbox.robotState = State.placing;
-                if(!Blackbox.isAligning())
+                if(!Blackbox.isAligning() && Blackbox.reefLevel != Blackbox.ReefScoreLevel.L4) //l4 goes really high, so let's wait until we are close
                     Blackbox.robotState = State.placing;
                 break;
             case placing:
