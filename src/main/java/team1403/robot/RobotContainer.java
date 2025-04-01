006d3c1a8b504d0eda8e3cada673be563924c491
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2025-04-01 19:00:16 -0400

    state machine go brrr

--- src/main/java/team1403/robot/RobotContainer.java
+++ src/main/java/team1403/robot/RobotContainer.java
@@ -127,9 +127,8 @@ public class RobotContainer {
       m_autoChooser.addOption("Swerve SysID QR", m_swerve.sysIdQuasistatic(Direction.kReverse));
       m_autoChooser.addOption("Swerve SysID DF", m_swerve.sysIdDynamic(Direction.kForward));
       m_autoChooser.addOption("Swerve SysID DR", m_swerve.sysIdDynamic(Direction.kReverse));
+      m_autoChooser.addOption("Drive Wheel Characterization", new DriveWheelCharacterization(m_swerve));
     }
-
-    m_autoChooser.addOption("Drive Wheel Characterization", new DriveWheelCharacterization(m_swerve));
 
     // autoChooser.addOption("Choreo Auto", AutoUtil.loadChoreoAuto("test", m_swerve));
     // autoChooser.addOption("FivePieceCenter", AutoHelper.getFivePieceAuto(m_swerve));
