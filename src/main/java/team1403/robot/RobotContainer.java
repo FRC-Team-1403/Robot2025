2d41a9dc585db876fe194f560ad39e3345aea666
CougarRoboticsProg1 <cougarroboticsprog1@gmail.com>
2025-04-04 11:13:59 -0400

    changed timeout

--- src/main/java/team1403/robot/RobotContainer.java
+++ src/main/java/team1403/robot/RobotContainer.java
@@ -147,7 +147,7 @@ public class RobotContainer {
   }
 
   private Command getAlignCommand(ReefSelect select) {
-    double timeout = 1.8;
+    double timeout = 1.7;
     Command vibrationCmd = new ControllerVibrationCommand(m_driverController.getHID(), 0.28, 1);
     return Commands.sequence(
       Blackbox.setAligningCmd(true),
