08b55bfbeb43c5b9486b7a6f7ebfd99296898008
Arrjuna P <105677807+ooantavaoo@users.noreply.github.com>
2025-11-13 13:07:51 -0500

    New Commands for practice wrist subsystem

--- src/main/java/team1403/robot/subsystems/AlgaeWristSubsystem.java
+++ src/main/java/team1403/robot/subsystems/AlgaeWristSubsystem.java
@@ -11,6 +11,7 @@ import com.revrobotics.spark.SparkBase.ResetMode;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Encoder;
@@ -23,11 +24,14 @@ public class AlgaeWristSubsystem extends SubsystemBase {
    private final SparkMax m_wristMotor;
    private final ProfiledPIDController m_wristPID;
    private double angleSetpoint;
+   private final SimpleMotorFeedforward m_Feedforward;
+   private double positionValue; 
 
     public AlgaeWristSubsystem() {
         m_wristMotor = new SparkMax(Constants.CanBus.algaeWristMotorID, MotorType.kBrushless);
         m_wristPID = new ProfiledPIDController(Constants.AlgaeWrist.Kp, Constants.AlgaeWrist.Ki, Constants.AlgaeWrist.Kd, new TrapezoidProfile.Constraints(Constants.AlgaeIntake.maxVelo, Constants.AlgaeIntake.maxAccel));
-
+        m_Feedforward = new SimpleMotorFeedforward(Constants.Wrist.WristKS, Constants.Wrist.WristKS, 0); //change later
+        positionValue = m_wristMotor.getAbsoluteEncoder().getPosition();
         SparkMaxConfig wConfig = new SparkMaxConfig();
         wConfig.smartCurrentLimit(40);
         m_wristMotor.configure(wConfig,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
@@ -38,11 +42,14 @@ public class AlgaeWristSubsystem extends SubsystemBase {
         m_wristPID.setGoal(angleSetpoint);
     }   
 
-    public void defaultWristCommand() { if (m_wristMotor.getAbsoluteEncoder().getPosition() - angleSetpoint > 0.1) { setWristAngle(angleSetpoint); } }
+    public void defaultWristCommand() { if (positionValue - angleSetpoint > 0.1) { setWristAngle(angleSetpoint); } }
+
+   
 
     @Override
     public void periodic() {
-        
+        defaultWristCommand();
+        m_wristPID.calculate(angleSetpoint, m_Feedforward.getKa());
     }
 
 }
